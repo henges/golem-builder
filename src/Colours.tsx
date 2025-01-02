45d2621a04f7b9434871dd5d3bef2855db7b40ed
U-LAPTOP-4EP3DB2K\alexa <spiderland@outlook.com>
2025-01-02 16:50:27 +0800

    .....vite nit.

--- src/Colours.tsx
+++ src/Colours.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {Text} from '@chakra-ui/react';
-import { IsQudShaderType, QudShader, QudShaderTypes, textShaders } from "./ShaderData";
+import { IsQudShaderType, QudShader, textShaders } from "./ShaderData";
 
 export type Color = {
     r: number;
